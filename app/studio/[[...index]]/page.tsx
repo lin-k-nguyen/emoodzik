4a6b894359991a962dd9d5acf5104bc28414a11f
Linh K Nguyen <linhknguyen@Admins-MacBook-Pro.local>
2026-06-28 15:20:17 +1000

    fix studio page

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -3,7 +3,7 @@
 export const dynamic = 'force-dynamic'
 
 import { NextStudio } from 'next-sanity/studio'
-import config from '@/sanity.config'
+import config from '../../../sanity.config'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
