33b14778fc8e5de2167325cdf35036a0def276b2
Linh K Nguyen <linhknguyen@Admins-MacBook-Pro.local>
2026-06-28 15:28:09 +1000

    fix studio import order

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-export const dynamic = 'force-dynamic'
-
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
+
+export const dynamic = 'force-dynamic'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
